3a60a871088c4e0eb92d77544597888f62fe5f9d
Jonathan Cao <janus21cn@gmail.com>
2016-04-21 16:53:39 -0400

    by Jon

--- app/src/main/java/wonder4/wordplay2/MainActivity.java
+++ app/src/main/java/wonder4/wordplay2/MainActivity.java
@@ -1,9 +1,10 @@
 package wonder4.wordplay2;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
