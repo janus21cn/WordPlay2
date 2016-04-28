485bf524024d7e2993d1a384a365b8480fcb6b71
Jonathan Cao <janus21cn@gmail.com>
2016-04-28 13:41:06 -0400

    added a basic home page

---
+++ app/src/main/java/wonder4/wordplay2/DrawingPage.java
@@ -0,0 +1,21 @@
+package wonder4.wordplay2;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jonathan on 4/28/2016.
+ */
+public class DrawingPage extends View {
+
+    public DrawingPage (Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+
+    }
+}
