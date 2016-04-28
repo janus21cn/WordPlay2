466f1d75872f9f7ef8e529546b43633e635dcc1c
JY2016 <yinj2@montclair.edu>
2016-04-28 18:18:14 -0400

    Jie

--- app/src/main/java/wonder4/wordplay2/WordFive.java
+++ app/src/main/java/wonder4/wordplay2/WordFive.java
@@ -6,6 +6,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ public class WordFive extends AppCompatActivity {
     private SensorManager sensorManager;
     private ImageView imgView;
     private Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,50 +30,32 @@ public class WordFive extends AppCompatActivity {
 
         initWithLight();
 
-        Button homeBtn = (Button) findViewById(R.id.btn_home5);
-        homeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        final Intent mainIntent = new Intent(WordFive.this, WordOne.class);
-                        WordFive.this.startActivity(mainIntent);
-                       WordFive.this.finish();
-                    }
-                }, 1000);
-            }
-        });
-
+        imgView = (ImageView)findViewById(R.id.imageView5);
         Button leftBtn = (Button) findViewById(R.id.btn_left5);
         leftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        final Intent mainIntent = new Intent(WordFive.this, MainActivity.class);
-                        WordFive.this.startActivity(mainIntent);
-                        WordFive.this.finish();
-                    }
-                }, 1000);
+                Intent intent = new Intent(WordFive.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button homeBtn = (Button) findViewById(R.id.btn_home5);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WordFive.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
-        Button rightBtn = (Button) findViewById(R.id.btn_right);
-        rightBtn.setOnClickListener(new View.OnClickListener() {
+        final MediaPlayer catsound = MediaPlayer.create(WordFive.this, R.raw.cat);
+        Button englishBtn = (Button) findViewById(R.id.btn_english5);
+        englishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        final Intent mainIntent = new Intent(WordFive.this, WordOne.class);
-                        WordFive.this.startActivity(mainIntent);
-                        WordFive.this.finish();
-                    }
-                }, 1000);
-            }
+                catsound.start();            }
         });
+
     }
 
     @Override
@@ -92,18 +76,19 @@ public class WordFive extends AppCompatActivity {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
             float value = sensorEvent.values[0];
-            RelativeLayout wordone = (RelativeLayout)findViewById(R.id.wordone);
-            imgView = (ImageView)findViewById(R.id.imageView5);
-            if(value<100){
+            RelativeLayout wordfive = (RelativeLayout)findViewById(R.id.wordfive);
+            if(value<50){
                 animation = AnimationUtils.loadAnimation(WordFive.this, R.anim.translate);
                 imgView.startAnimation(animation);
+
             }else{
-                wordone.setBackgroundResource(R.drawable.bg2);
+
             }
         }
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int i) {
+
         }
     };
 }
--- app/src/main/java/wonder4/wordplay2/WordOne.java
+++ app/src/main/java/wonder4/wordplay2/WordOne.java
@@ -1,12 +1,16 @@
 package wonder4.wordplay2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -20,6 +24,32 @@ public class WordOne extends AppCompatActivity {
         setContentView(R.layout.activity_word_one);
 
         initWithLight();
+
+        Button leftBtn = (Button) findViewById(R.id.btn_left);
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WordOne.this, WordFive.class);
+                startActivity(intent);
+            }
+        });
+        Button homeBtn = (Button) findViewById(R.id.btn_home);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WordOne.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final MediaPlayer catsound = MediaPlayer.create(WordOne.this, R.raw.cat);
+        Button englishBtn = (Button) findViewById(R.id.btn_english);
+        englishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                catsound.start();            }
+        });
+
     }
 
     @Override
@@ -41,7 +71,7 @@ public class WordOne extends AppCompatActivity {
         public void onSensorChanged(SensorEvent sensorEvent) {
             float value = sensorEvent.values[0];
             RelativeLayout wordone = (RelativeLayout)findViewById(R.id.wordone);
-            if(value<100){
+            if(value<50){
                 wordone.setBackgroundResource(R.drawable.word1c);
             }else{
                 wordone.setBackgroundResource(R.drawable.word1);
