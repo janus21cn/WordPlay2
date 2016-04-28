70bc55faae96e6c353ca0ad8aae20fae549842c2
JY2016 <yinj2@montclair.edu>
2016-04-28 16:28:02 -0400

    Jie

--- app/src/main/java/wonder4/wordplay2/MainActivity.java
+++ app/src/main/java/wonder4/wordplay2/MainActivity.java
@@ -1,8 +1,11 @@
 package wonder4.wordplay2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button playBtn = (Button) findViewById(R.id.btn_Play);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WordOne.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
